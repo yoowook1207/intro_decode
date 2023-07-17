85d911d1ac6aca8c18fc9443b74a53e931009a0f
Sung Yoo <yoowook1207@gmail.com>
2023-07-17 00:10:12 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { FlyerComponent } from './flyer/flyer.component';
+import { HeroComponent } from './section/hero/hero.component';
+import { BlockComponent } from './section/block/block.component';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlyerComponent,
+    HeroComponent,
+    BlockComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
