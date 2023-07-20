f42a962834b9cd8081983714c46baf06a1e0427d
Sung Yoo <yoowook1207@gmail.com>
2023-07-19 20:02:23 -0400

    education video added

--- src/app/flyer/flyer.component.ts
+++ src/app/flyer/flyer.component.ts
@@ -14,7 +14,7 @@ export class FlyerComponent implements OnInit{
 
   thirdBlockInfo = {
     topper: '단 시간안에 창조하는 세계관',
-    inputText: 'Unlock the power of instant creation  ',
+    inputText: 'Unlock the power of instant creation.',
     inputImgName: 'book2.mp4',
     isVideo: true
   }
