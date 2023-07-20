b9db26d6e13f41ec38e0840fd7471505f8a35f90
Sung Yoo <yoowook1207@gmail.com>
2023-07-20 09:57:29 -0400

    picture changed

--- src/app/flyer/flyer.component.ts
+++ src/app/flyer/flyer.component.ts
@@ -8,14 +8,14 @@ import {Component, OnInit, } from '@angular/core';
 export class FlyerComponent implements OnInit{
   secondBlockInfo = {
     topper: '혁신적인 영어교육',
-    inputText: 'Welcome to a new era of AI education.',
+    inputText: 'Welcome to a new era of AI education',
     inputImgName: 'AIeducation.mp4',
     isVideo:true
   }
 
   thirdBlockInfo = {
     topper: '단 시간안에 창조하는 세계관',
-    inputText: 'Unlock the power of instant creation.',
+    inputText: 'Unlock the power of instant creation',
     inputImgName: 'book2.mp4',
     isVideo: true
   }
@@ -23,19 +23,19 @@ export class FlyerComponent implements OnInit{
   inputArray:any[]= [
     {
     topper: '영어 읽기 능력 향상',
-    inputText: 'Ignite your reading skills with our AI-powered platform.',
+    inputText: 'Ignite your reading skills with our AI-powered platform',
     inputImgName: 'img3.png',
     isMiddle: false
     },
     {
       topper: 'Create with AI',
-      inputText: 'Captivating visuals that brings stories to life.',
+      inputText: 'Captivating visuals that brings stories to life',
       inputImgName: 'img4.png',
       isMiddle: true
     },
     {
       topper: '넓은 세계를 둘러보세요',
-      inputText: `Join our vibrant community, share, and discover new worlds.`,
+      inputText: `Join our vibrant community, share, and discover new worlds`,
       inputImgName: 'img5.png',
       isMiddle: false
     },
@@ -43,7 +43,7 @@ export class FlyerComponent implements OnInit{
 
   seventhBlockInfo = {
     topper: 'Are you ready?',
-    inputText: 'Experience transformative reading like never before.',
+    inputText: 'Experience transformative reading like never before',
     inputImgName: 'walking.mp4',
     isVideo: true,
     isLast: true
